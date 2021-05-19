887e3d14e928465bf06d8db4b791d33be77c2c12
Hunter Saunders <4897hunter@gmail.com>
2021-05-19 11:43:08 -0600

    Added a Reply

--- src/mundt/Main.java
+++ src/mundt/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	String hello = "Hello, my name is Hailey.";
+	String reply = "Hello Hailey, my name is Hunter";
 	System.out.println(hello);
+	System.out.println(reply);
     }
 }
