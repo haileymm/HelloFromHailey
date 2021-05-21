34856c9718ee5a09b86ee1279e5f50f9dd61f594
Hailey <gri06001@byui.edu>
2021-05-21 15:47:36 -0700

    change on main

--- src/mundt/Main.java
+++ src/mundt/Main.java
@@ -7,5 +7,6 @@ public class Main {
 	String reply = "Hello Hailey, my name is Hunter";
 	System.out.println(hello);
 	System.out.println(reply);
+	System.out.println("This is a change made for the stretch activity.");
     }
 }
